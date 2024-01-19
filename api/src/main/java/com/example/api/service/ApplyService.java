5aa5fa0aec4071602b8fa19c971aa79a1008acf3
choi-sangyong <csy7792@naver.com>
2024-01-20 03:14:53 +0900

    project init

---
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -0,0 +1,46 @@
+package com.example.api.service;
+
+import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
+import com.example.api.repository.CouponCountRepository;
+import com.example.api.repository.CouponRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplyService {
+
+    private final CouponRepository couponRepository;
+
+    private final CouponCountRepository couponCountRepository;
+
+    private final CouponCreateProducer couponCreateProducer;
+
+    private final AppliedUserRepository appliedUserRepository;
+
+    public ApplyService(
+            CouponRepository couponRepository,
+            CouponCountRepository couponCountRepository,
+            CouponCreateProducer couponCreateProducer,
+            AppliedUserRepository appliedUserRepository) {
+        this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
+    }
+
+    public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            return;
+        }
+
+        Long count = couponCountRepository.increment();
+
+        if (count > 100) {
+            return;
+        }
+
+        couponCreateProducer.create(userId);
+    }
+}
---
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,83 @@
+package com.example.api.service;
+
+import com.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    public void 한번만응모() {
+        applyService.apply(1L);
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Thread.sleep(10000);
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(100);
+    }
+
+    @Test
+    public void 한명당_한개의쿠폰만_발급() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Thread.sleep(10000);
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+}
